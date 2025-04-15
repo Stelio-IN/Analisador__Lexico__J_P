8cbf3b58610c80893cc092b1d807da9144625e30
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2025-04-15 23:23:27 +0200

    asdsad

--- Analisador_Lexico_Java/src/controller/Teste.java
+++ Analisador_Lexico_Java/src/controller/Teste.java
@@ -11,5 +11,8 @@ package controller;
 public class Teste {
     public static void main(String[] args) {
         System.out.println("hello wosadasdsad");
+            System.out.println("hello wosadasdsad");
+                System.out.println("hello wosadasdsad");
+                    System.out.println("hello wosadasdsad");
     }
 }
